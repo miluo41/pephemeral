a13b52aeb75064087c434e266dc28108cfebc6da
Luo Mi <miluo@g.ucla.edu>
2017-11-14 11:36:21 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask, render_template, request
-from wtforms import Form, FloatField, validators,TextField, widgets, SelectField
-from compute import compute
-app = Flask(__name__)
-from flaskexample import views
